5ec389691b5690073390aa58cf3ce7cb4be63122
Max Bradley <maxbradley64@gmail.com>
2021-06-20 18:34:12 +0100

    Added IServiceCollection extensions for adding required dependencies

---
+++ Abstractions.DateAndTime/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Abstractions.DateAndTime.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Abstractions.DateAndTime.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDateTimeService(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddSingleton<IDateTimeService, DateTimeService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddDateTimeOffsetService(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddSingleton<IDateTimeOffsetService, DateTimeOffsetService>();
+
+            return services;
+        }
+    }
+}
