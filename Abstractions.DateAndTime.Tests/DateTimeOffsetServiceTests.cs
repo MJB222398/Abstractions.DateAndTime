3b7e73c76b3fe9861bfbed70641e66ded9a6adf3
Max Bradley <maxbradley64@gmail.com>
2021-06-20 19:10:31 +0100

    Added unit tests for the DateTimeOffset abstraction

---
+++ Abstractions.DateAndTime.Tests/DateTimeOffsetServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using Abstractions.DateAndTime.Services;
+using FakeItEasy;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Abstractions.DateAndTime.Tests
+{
+    public class DateTimeOffsetServiceTests
+    {
+        private readonly IDateTimeOffsetService _dateTimeOffsetService;
+
+        public DateTimeOffsetServiceTests()
+        {
+            _dateTimeOffsetService = A.Fake<IDateTimeOffsetService>();
+        }
+
+        [Test]
+        public void IDateTimeOffsetService_AbstractsAllStaticDateTimeOffsetProperties()
+        {
+            var dateTimeOffsetStaticProperties = typeof(DateTimeOffset).GetProperties(BindingFlags.Public | BindingFlags.Static);
+            var dateTimeOffsetServiceMethods = typeof(IDateTimeOffsetService).GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .Where(m => m.GetParameters().Length == 0);
+
+            Assert.That(
+                dateTimeOffsetStaticProperties.Select(m => m.Name),
+                Is.SubsetOf(dateTimeOffsetServiceMethods.Select(m => m.Name)),
+                $"Not all static {nameof(DateTimeOffset)} properties are abstracted.");
+
+            foreach (var property in dateTimeOffsetStaticProperties)
+            {
+                var abstraction = dateTimeOffsetServiceMethods.Single(m => m.Name == property.Name);
+
+                Assert.That(
+                    property.PropertyType,
+                    Is.EqualTo(abstraction.ReturnType),
+                    $"'{property.Name}' abstraction return type: '{abstraction.ReturnType}' does not match that of the {nameof(DateTimeOffset)} property: '{property.PropertyType}'.");
+            }
+        }
+
+        [Test]
+        public void IDateTimeOffsetService_DateTimeOffsetNowCanBeFaked()
+        {
+            // Arrange
+            var pacificStandardTimeUtcOffset = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time").BaseUtcOffset;
+            var targetDateTimeOffset = new DateTimeOffset(2021, 01, 02, 12, 34, 56, pacificStandardTimeUtcOffset);
+            A.CallTo(() => _dateTimeOffsetService.Now()).Returns(targetDateTimeOffset);
+
+            // Act
+            var fakedDateTimeOffsetNow = _dateTimeOffsetService.Now();
+
+            // Assert
+            Assert.That(
+                fakedDateTimeOffsetNow,
+                Is.EqualTo(targetDateTimeOffset),
+                $"{nameof(IDateTimeOffsetService)}.{nameof(IDateTimeOffsetService.Now)} was not successfully faked.");
+        }
+
+        [Test]
+        public void IDateTimeOffsetService_DateTimeOffsetUtcNowCanBeFaked()
+        {
+            // Arrange
+            var tokyoStandardTimeUtcOffset = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time").BaseUtcOffset;
+            var targetDateTimeOffset = new DateTimeOffset(2021, 01, 02, 12, 34, 56, tokyoStandardTimeUtcOffset);
+            A.CallTo(() => _dateTimeOffsetService.UtcNow()).Returns(targetDateTimeOffset);
+
+            // Act
+            var fakedDateTimeOffsetUtcNow = _dateTimeOffsetService.UtcNow();
+
+            // Assert
+            Assert.That(
+                fakedDateTimeOffsetUtcNow,
+                Is.EqualTo(targetDateTimeOffset),
+                $"{nameof(IDateTimeOffsetService)}.{nameof(IDateTimeOffsetService.UtcNow)} was not successfully faked.");
+        }
+    }
+}
